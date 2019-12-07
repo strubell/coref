50aa8e9b0bc6f628eb7fcd9c88ab9a7cd263ffa5
Emma Strubell <strubell@fb.com>
2019-12-07 15:45:13 -0500

    eval on preco

--- mentions_eval_slurm.sh
+++ mentions_eval_slurm.sh
@@ -36,67 +36,66 @@ for cname in train_spanbert_base_conll12 \
 #             train_spanbert_large_preco_mult_gold ;
 do
   for top_span_ratio in 0.4 0.8; do
-    echo $top_span_ratio
-      MEM="32g"
-      SAVE="${SAVE_ROOT}/${cname}"
-      mkdir -p ${SAVE}
-      SCRIPT="${JOBSCRIPTS}/mentions-eval-${top_span_ratio}.${cname}.sh"
-      SLURM="${JOBSCRIPTS}/mentions-eval-${top_span_ratio}.${cname}.slrm"
-      output_prefix="$SAVE/mentions-eval-${top_span_ratio}-$timestamp"
-      echo "#!/bin/sh" > ${SCRIPT}
-      echo "#!/bin/sh" > ${SLURM}
-  #    echo "source activate py36" >> ${SLURM}
-      echo "#SBATCH --job-name=eval-$cname" >> ${SLURM}
-      echo "#SBATCH --output=$output_prefix.out" >> ${SLURM}
-      echo "#SBATCH --error=$output_prefix.err" >> ${SLURM}
-      echo "#SBATCH --signal=USR1@120" >> ${SLURM}
-      echo "#SBATCH --partition=${queue}" >> ${SLURM}
-    #    echo "#SBATCH --comment=ICRLDEADLINE" >> ${SLURM}
-    #    echo "#SBATCH --nodes=${num_nodes}" >> ${SLURM}
-    #    echo "#SBATCH --ntasks-per-node=8" >> ${SLURM}
-      echo "#SBATCH --mem=$MEM" >> ${SLURM}
-      echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-  #    echo "#SBATCH --cpus-per-task 8" >> ${SLURM}
-      echo "#SBATCH --time=72:00:00" >> ${SLURM}
+    MEM="32g"
+    SAVE="${SAVE_ROOT}/${cname}"
+    mkdir -p ${SAVE}
+    SCRIPT="${JOBSCRIPTS}/mentions-eval-${top_span_ratio}.${cname}.sh"
+    SLURM="${JOBSCRIPTS}/mentions-eval-${top_span_ratio}.${cname}.slrm"
+    output_prefix="$SAVE/mentions-eval-${top_span_ratio}-$timestamp"
+    echo "#!/bin/sh" > ${SCRIPT}
+    echo "#!/bin/sh" > ${SLURM}
+#    echo "source activate py36" >> ${SLURM}
+    echo "#SBATCH --job-name=eval-$cname" >> ${SLURM}
+    echo "#SBATCH --output=$output_prefix.out" >> ${SLURM}
+    echo "#SBATCH --error=$output_prefix.err" >> ${SLURM}
+    echo "#SBATCH --signal=USR1@120" >> ${SLURM}
+    echo "#SBATCH --partition=${queue}" >> ${SLURM}
+  #    echo "#SBATCH --comment=ICRLDEADLINE" >> ${SLURM}
+  #    echo "#SBATCH --nodes=${num_nodes}" >> ${SLURM}
+  #    echo "#SBATCH --ntasks-per-node=8" >> ${SLURM}
+    echo "#SBATCH --mem=$MEM" >> ${SLURM}
+    echo "#SBATCH --gres=gpu:1" >> ${SLURM}
+#    echo "#SBATCH --cpus-per-task 8" >> ${SLURM}
+    echo "#SBATCH --time=72:00:00" >> ${SLURM}
 
-      # this is the correct file for bert_large and spanbert_base
-      dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.384.jsonlines
+    # this is the correct file for bert_large and spanbert_base
+    dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.384.jsonlines
 
-      if [[ $cname =~ "large" ]]; then
-        if [[ $cname =~ "_spanbert_" ]]; then
-          # spanbert_large
-          dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.512.jsonlines
-        fi
-      else
-        if [[ $cname =~ "_bert_" ]]; then
-          # bert_base
-          dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.128.jsonlines
-        fi
+    if [[ $cname =~ "large" ]]; then
+      if [[ $cname =~ "_spanbert_" ]]; then
+        # spanbert_large
+        dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.512.jsonlines
       fi
-
-      out_file="$PRED_ROOT/detected-mentions_${cname}_$top_span_ratio.jsonl"
-
-      if [[ $cname =~ "large" ]]; then
-          echo "#SBATCH --constraint=volta32gb" >> ${SLURM}
+    else
+      if [[ $cname =~ "_bert_" ]]; then
+        # bert_base
+        dev_file=/private/home/strubell/research/data/spanbert_data_clean/dev.english.128.jsonlines
       fi
-      echo "srun sh ${SCRIPT}" >> ${SLURM}
+    fi
+
+    out_file="$PRED_ROOT/detected-mentions_${cname}_$top_span_ratio.jsonl"
+
+    if [[ $cname =~ "large" ]]; then
+        echo "#SBATCH --constraint=volta32gb" >> ${SLURM}
+    fi
+    echo "srun sh ${SCRIPT}" >> ${SLURM}
 #      echo "cp ${SAVE}/preds.conll $PRED_ROOT/preds-$cname.conll" >> ${SLURM}
 
-  #    echo "source activate py36" >> ${SCRIPT}
-      echo "echo \$SLURM_JOB_ID >> jobs" >> ${SCRIPT}
-      echo "{ " >> ${SCRIPT}
-      echo "echo $cname " >> ${SCRIPT}
-      echo "cd $PROJ_ROOT" >> ${SCRIPT}
-      # mentions_and_scores.py models/train_spanbert_base_conll12 ~/research/spanbert_data_clean/dev.english.384.jsonlines 0.8 detected-mentions_spanbert-base_conll-dev_0-8.jsonl
-      echo "python3 -O mentions_and_scores.py $cname $dev_file $top_span_ratio $out_file" >> ${SCRIPT}
-      echo "kill -9 \$\$" >> ${SCRIPT}
-      echo "} & " >> ${SCRIPT}
-      echo "child_pid=\$!" >> ${SCRIPT}
-      echo "trap \"echo 'TERM Signal received';\" TERM" >> ${SCRIPT}
-      echo "trap \"echo 'Signal received'; if [ \"\$SLURM_PROCID\" -eq \"0\" ]; then sbatch ${SLURM}; fi; kill -9 \$child_pid; \" USR1" >> ${SCRIPT}
-      echo "while true; do     sleep 1; done" >> ${SCRIPT}
-      echo "Created scripts: ${SLURM} ${SCRIPT}"
-      echo "Writing output: $output_prefix.out"
-      sbatch ${SLURM}
+#    echo "source activate py36" >> ${SCRIPT}
+    echo "echo \$SLURM_JOB_ID >> jobs" >> ${SCRIPT}
+    echo "{ " >> ${SCRIPT}
+    echo "echo $cname " >> ${SCRIPT}
+    echo "cd $PROJ_ROOT" >> ${SCRIPT}
+    # mentions_and_scores.py models/train_spanbert_base_conll12 ~/research/spanbert_data_clean/dev.english.384.jsonlines 0.8 detected-mentions_spanbert-base_conll-dev_0-8.jsonl
+    echo "python3 -O mentions_and_scores.py $cname $dev_file $top_span_ratio $out_file" >> ${SCRIPT}
+    echo "kill -9 \$\$" >> ${SCRIPT}
+    echo "} & " >> ${SCRIPT}
+    echo "child_pid=\$!" >> ${SCRIPT}
+    echo "trap \"echo 'TERM Signal received';\" TERM" >> ${SCRIPT}
+    echo "trap \"echo 'Signal received'; if [ \"\$SLURM_PROCID\" -eq \"0\" ]; then sbatch ${SLURM}; fi; kill -9 \$child_pid; \" USR1" >> ${SCRIPT}
+    echo "while true; do     sleep 1; done" >> ${SCRIPT}
+    echo "Created scripts: ${SLURM} ${SCRIPT}"
+    echo "Writing output: $output_prefix.out"
+    sbatch ${SLURM}
   done
 done
