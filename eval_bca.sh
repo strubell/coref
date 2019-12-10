65652720528e8a1ff3a75e0583751082e1b4df41
Emma Strubell <strubell@fb.com>
2019-12-09 16:36:12 -0800

    eval bca

--- eval_bca.sh
+++ eval_bca.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+scorer_dir="reference-coreference-scorers"
 bca_dir="/mnt/nfs/work1/mccallum/coref/bca-output/"
 
 model_output_dir="best_spanbert_pred_outputs"
@@ -13,6 +14,6 @@ out_dir="bca_scores"
 for metric in bcub ceafe muc lea; do
   for corrected_file in $(ls "$current_bca_dir/output.corrected.*"); do
     just_fname=${corrected_file##*/}
-    perl reference-coreference-scorers/scorer.pl $metric $key $corrected_file > $out_dir/$just_fname.eval.$metric
+    perl $scorer_dir/scorer.pl $metric $key $corrected_file > $out_dir/$just_fname.eval.$metric
   done
 done
