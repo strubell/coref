1e53750eab9d91ba35124921d2aae4f7d5320676
Emma Strubell <strubell@fb.com>
2019-12-09 17:08:51 -0800

    eval bca

--- eval_bca.sh
+++ eval_bca.sh
@@ -12,10 +12,29 @@ key="$current_bca_dir/output.gold"
 out_dir="bca_scores"
 mkdir -p $out_dir
 
-for metric in bcub ceafe muc lea; do
-  for corrected_file in $(ls $current_bca_dir/output.corrected.*); do
-    echo $metric $corrected_file
-    just_fname=${corrected_file##*/}
-    perl $scorer_dir/scorer.pl $metric $key $corrected_file > $out_dir/$just_fname.eval.$metric
-  done
+#for metric in bcub ceafe muc lea; do
+#  for corrected_file in $(ls $current_bca_dir/output.corrected.*); do
+#    echo $metric $corrected_file
+#    just_fname=${corrected_file##*/}
+#    perl $scorer_dir/scorer.pl $metric $key $corrected_file > $out_dir/$just_fname.eval.$metric
+#  done
+#done
+
+for corrected_file in $(ls $current_bca_dir/output.corrected.*); do
+  mention_score_outfile=$out_dir/mentions.scores
+  cat /dev/null > $mention_score_outfile
+  just_fname=${corrected_file##*/}
+  scores=$(grep "Identification of Mentions: Recall:" $out_dir/$just_fname.eval.lea | \
+           sed 's/[(%)]//g' | \
+           awk '{print $5"\t"$7"\t"$8"\t"$10"\t"$12"\t"$13"\t"$15}')
+  echo $just_fname"\t"$scores >> $mention_score_outfile
+
+  for metric in bcub ceafe muc lea; do
+      metric_score_outfile=$out_dir/scores.$metric
+      cat /dev/null > $metric_score_outfile
+      scores=$(grep "Coreference: Recall:" $out_dir/$just_fname.eval.$metric | \
+      sed 's/[(%)]//g' | \
+      awk '{print $5"\t"$7"\t"$8"\t"$10"\t"$12"\t"$13"\t"$15}')
+      echo $just_fname"\t" $scores >> $metric_score_outfile
+    done
 done
